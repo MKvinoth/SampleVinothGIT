7168876e6f0ce128c4e052a3ae3147aef55dfb2b
MKvinoth <Vinoth10rko@gmail.com>
2022-10-06 10:31:04 +0530

    firstgit

---
+++ src/org/sample/Employee.java
@@ -0,0 +1,27 @@
+package org.sample;
+
+public class Employee {
+	
+	
+	
+	private void a() {
+
+		System.out.println("Balaji");
+	}
+	private void b() {
+	
+	System.out.println("Vinoth");
+	
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		Employee ss = new Employee();
+		ss.a();
+		ss.b();
+		
+	}
+	
+
+}
